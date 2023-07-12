8174b29b2a93794b627e64755aa92a8c0f488249
Andrei Chekmasov <andrei.chekmasov@softwire.com>
2023-07-12 14:02:40 +0100

    Add king movement

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -6,7 +6,7 @@ export default class Bishop extends Piece {
     super(player);
   }
 
-  checkInDirection(board, startSquare, rowChange, colChange, available_moves) {
+  checkInDirection(board, startSquare, rowChange, colChange, availableMoves) {
     let checkSquare = Square.copy(startSquare);
     checkSquare.row += rowChange;
     checkSquare.col += colChange;
@@ -14,27 +14,27 @@ export default class Bishop extends Piece {
       let pieceAtSquare = board.getPiece(checkSquare);
       if (!!pieceAtSquare) {
         if (this.canCapture(pieceAtSquare))
-          available_moves.push(Square.copy(checkSquare));
+          availableMoves.push(Square.copy(checkSquare));
         break;
       }
-      available_moves.push(Square.copy(checkSquare));
+      availableMoves.push(Square.copy(checkSquare));
       checkSquare.row += rowChange;
       checkSquare.col += colChange;
     }
   }
 
   getAvailableMoves(board) {
-    let piece_square = board.findPiece(this);
-    let available_moves = [];
+    let pieceSquare = board.findPiece(this);
+    let availableMoves = [];
     // Checking in the +1,+1 direction
-    this.checkInDirection(board, piece_square, 1, 1, available_moves);
+    this.checkInDirection(board, pieceSquare, 1, 1, availableMoves);
     // Checking in the +1,-1 direction
-    this.checkInDirection(board, piece_square, 1, -1, available_moves);
+    this.checkInDirection(board, pieceSquare, 1, -1, availableMoves);
     // Checking in the -1,+1 direction
-    this.checkInDirection(board, piece_square, -1, 1, available_moves);
+    this.checkInDirection(board, pieceSquare, -1, 1, availableMoves);
     // Checking in the -1,-1 direction
-    this.checkInDirection(board, piece_square, -1, -1, available_moves);
+    this.checkInDirection(board, pieceSquare, -1, -1, availableMoves);
 
-    return available_moves;
+    return availableMoves;
   }
 }
--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,11 +1,26 @@
-import Piece from './piece';
+import Square from "../square";
+import Piece from "./piece";
 
 export default class King extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+  getAvailableMoves(board) {
+    let pieceSquare = board.findPiece(this);
+    let available_moves = [];
+    for (let rowChange = -1; rowChange <= 1; rowChange++) {
+      for (let colChange = -1; colChange <= 1; colChange++) {
+        if (rowChange == 0 && colChange == 0) continue;
+        let checkSquare = Square.copy(pieceSquare);
+        checkSquare.row += rowChange;
+        checkSquare.col += colChange;
+        if (!board.isInBounds(checkSquare)) continue;
+        let pieceAtSquare = board.getPiece(checkSquare);
+        if (!!pieceAtSquare && !this.canCapture(pieceAtSquare)) continue;
+        available_moves.push(checkSquare);
+      }
     }
+    return available_moves;
+  }
 }
