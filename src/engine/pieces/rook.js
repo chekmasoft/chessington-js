68087406cbf6894b9247bb2492c6de55228b3557
Andrei Chekmasov <andrei.chekmasov@softwire.com>
2023-07-12 14:13:30 +0100

    Add rook tests and moves

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,11 +1,40 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Square from "../square";
 
 export default class Rook extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+  checkInDirection(board, startSquare, rowChange, colChange, availableMoves) {
+    let checkSquare = Square.copy(startSquare);
+    checkSquare.row += rowChange;
+    checkSquare.col += colChange;
+    while (board.isInBounds(checkSquare)) {
+      let pieceAtSquare = board.getPiece(checkSquare);
+      if (!!pieceAtSquare) {
+        if (this.canCapture(pieceAtSquare))
+          availableMoves.push(Square.copy(checkSquare));
+        break;
+      }
+      availableMoves.push(Square.copy(checkSquare));
+      checkSquare.row += rowChange;
+      checkSquare.col += colChange;
     }
+  }
+
+  getAvailableMoves(board) {
+    let pieceSquare = board.findPiece(this);
+    let availableMoves = [];
+    // Checking in the +1,0 direction
+    this.checkInDirection(board, pieceSquare, 1, 0, availableMoves);
+    // Checking in the -1,0 direction
+    this.checkInDirection(board, pieceSquare, -1, 0, availableMoves);
+    // Checking in the 0,+1 direction
+    this.checkInDirection(board, pieceSquare, 0, 1, availableMoves);
+    // Checking in the 0,-1 direction
+    this.checkInDirection(board, pieceSquare, 0, -1, availableMoves);
+
+    return availableMoves;
+  }
 }
