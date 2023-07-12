184cd5937e1a171274d1e4ad6c6f3ce8b49c371d
Andrei Chekmasov <andrei.chekmasov@softwire.com>
2023-07-12 14:15:29 +0100

    Add queen tests and moves

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,11 +1,44 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Square from "../square";
 
 export default class Queen extends Piece {
-    constructor(player) {
-        super(player);
+  constructor(player) {
+    super(player);
+  }
+
+  checkInDirection(board, startSquare, rowChange, colChange, availableMoves) {
+    let checkSquare = Square.copy(startSquare);
+    checkSquare.row += rowChange;
+    checkSquare.col += colChange;
+    while (board.isInBounds(checkSquare)) {
+      let pieceAtSquare = board.getPiece(checkSquare);
+      if (!!pieceAtSquare) {
+        if (this.canCapture(pieceAtSquare))
+          availableMoves.push(Square.copy(checkSquare));
+        break;
+      }
+      availableMoves.push(Square.copy(checkSquare));
+      checkSquare.row += rowChange;
+      checkSquare.col += colChange;
+    }
+  }
+
+  getAvailableMoves(board) {
+    let pieceSquare = board.findPiece(this);
+    let availableMoves = [];
+    for (let rowChange = -1; rowChange <= 1; rowChange++) {
+      for (let colChange = -1; colChange <= 1; colChange++) {
+        if (rowChange == 0 && colChange == 0) continue;
+        this.checkInDirection(
+          board,
+          pieceSquare,
+          rowChange,
+          colChange,
+          availableMoves
+        );
+      }
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
-    }
+    return availableMoves;
+  }
 }
